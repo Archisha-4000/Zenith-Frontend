1d42b6fa6e118a43bd9f0ac93566c8a69a2ef5f7
Archisha-4000 <archishaupadhyaya10d@gmail.com>
2025-06-20 19:31:21 +0530

    fixed tailwind error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "af11a15d-2743-463f-a42c-60405f89068f"
+});
+
+export default withCivicAuth(nextConfig)
