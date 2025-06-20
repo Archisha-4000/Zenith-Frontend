5e2a4c92b486e310074e78db74d5703b27c62085
Archisha-4000 <archishaupadhyaya10d@gmail.com>
2025-06-20 19:53:37 +0530

    relocated files

--- actions/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./organization";
-export * from "./user";
-export * from "./quest";
-export * from "./task";
-export * from "./auditLog"
-export * from "./device";
-export * from "./installerLink";
